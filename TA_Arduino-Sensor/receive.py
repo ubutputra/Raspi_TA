24be01df660a6d84ae26764a2a8a20cc64d1b706
ubutputra <ubut31@gmail.com>
2020-03-22 20:53:10 +0700

    coba list

--- TA_Arduino-Sensor/receive.py
+++ TA_Arduino-Sensor/receive.py
@@ -6,8 +6,10 @@ import spidev
 GPIO.setmode(GPIO.BCM)
 
 
-address = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [C,O,B,A]]
-pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1] ]
+#address = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [C,O,B,A]]
+pipes = [[0xF0, 0xF0, 0xF0, 0xF0, 0xA1], [0xF0, 0xF0, 0xF0, 0xF0, 0xA2] ]
+pipes2 = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1] ]
+
 
 radio = NRF24(GPIO, spidev.SpiDev())
 radio.begin(0, 17)
@@ -21,33 +23,50 @@ radio.setAutoAck(True)
 radio.enableDynamicPayloads()
 radio.enableAckPayload()
 
-#radio.openReadingPipe(1, pipes[1])
-radio.openReadingPipe(1, address[1])
+radio.openReadingPipe(1, pipes[0])
+radio.openReadingPipe(2, pipes[1])
+
+#radio.openReadingPipe(1, address[1])
 radio.printDetails()
 
 radio.startListening()
 count = 0 
 while True:
-    ackPL = [1]
+    #ackPL = [1]
     while not radio.available(0):
+        #print("timeout")
         time.sleep(1 / 1000)
-    receivedMessage = []
-    radio.read(receivedMessage, radio.getDynamicPayloadSize())
-    print(receivedMessage)
-    print("Received: {}".format(receivedMessage))
-    print("Translating the receivedMessage into unicode characters")
-    string = ""
-    for n in receivedMessage:
-        # Decode into standard unicode set
-        if (n >= 32 and n <= 126):
-            #print(n)
-            string += chr(n)
-    print(string)
-    #radio.writeAckPayload(1, ackPL, len(ackPL))
-    #print("Loaded payload reply of {}".format(ackPL))
-    count = count + 1
-    print("received message decodes to : {}".format(string))
-    print("pesan ke {}".format(count))
-    print(time.strftime('%X %x %Z'))
-    print("-->><<--")
+    
+    
+
+    if (radio.available(pipes[0])):
+        print("pipes 1 - node 1 >>>>>")
+    if (radio.available(pipes[1])):
+        print("pipes 2 - node 2 >>>>>")        
+        receivedMessage = []
+        radio.read(receivedMessage, radio.getDynamicPayloadSize())
+        print(radio.getDynamicPayloadSize())
+        print(receivedMessage)
+        print("Received: {}".format(receivedMessage))
+        print("Translating the receivedMessage into unicode characters")
+        string = ""
+        for n in receivedMessage:
+            # Decode into standard unicode set
+            if (n >= 32 and n <= 126):
+                #print(n)
+                string += chr(n)
+        print(string)
+        data = string.split("|")
+        print("list string : {}".format(data))
+        data = list(map(int,data))
+        print("list integer : {}".format(data))
+
+        
+        #radio.writeAckPayload(1, ackPL, len(ackPL))
+        #print("Loaded payload reply of {}".format(ackPL))
+        count = count + 1
+        print("received message decodes to : {}".format(string))
+        print("pesan ke {}".format(count))
+        print(time.strftime('%X %x %Z'))
+        print("-->><<--")
     
